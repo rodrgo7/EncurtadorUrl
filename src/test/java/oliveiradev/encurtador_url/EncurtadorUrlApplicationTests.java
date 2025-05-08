224569f3b58c0e7334fe9a0ed47f8b4168bbe1e2
Rodrgo <r.oliveira.95@hotmail.com>
2025-05-08 20:18:39 -0300

    FirstCommit

---
+++ src/test/java/oliveiradev/encurtador_url/EncurtadorUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package oliveiradev.encurtador_url;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncurtadorUrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
